da40aa81126d2c71c18e6fe1ed73a52ac01f766a
Sierk Beij <sierk@cococollective.org>
2016-11-29 20:52:24 -0800

    Cleaned up LocationForm.php

--- src/Form/LocationForm.php
+++ src/Form/LocationForm.php
@@ -4,6 +4,7 @@ namespace Drupal\locationentity\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\locationentity\LocationMessengerTrait;
 
 /**
  * Form controller for Location edit forms.
@@ -11,37 +12,29 @@ use Drupal\Core\Form\FormStateInterface;
  * @ingroup locationentity
  */
 class LocationForm extends ContentEntityForm {
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    /* @var $entity \Drupal\locationentity\Entity\Location */
-    $form = parent::buildForm($form, $form_state);
-    $entity = $this->entity;
 
-    return $form;
-  }
+  use LocationMessengerTrait;
+
+  /**
+   * The location being used by this form.
+   *
+   * @var \Drupal\locationentity\LocationInterface
+   */
+  protected $entity;
 
   /**
    * {@inheritdoc}
    */
   public function save(array $form, FormStateInterface $form_state) {
-    $entity = $this->entity;
-    $status = parent::save($form, $form_state);
+    parent::save($form, $form_state);
 
-    switch ($status) {
-      case SAVED_NEW:
-        drupal_set_message($this->t('Created the %label Location.', [
-          '%label' => $entity->label(),
-        ]));
-        break;
+    $this->addStatusMessage($this->t('Saved location <em>%label</em>.', [
+      '%label' => $this->entity->label(),
+    ]));
 
-      default:
-        drupal_set_message($this->t('Saved the %label Location.', [
-          '%label' => $entity->label(),
-        ]));
-    }
-    $form_state->setRedirect('entity.locationentity.canonical', ['locationentity' => $entity->id()]);
+    $form_state->setRedirect('entity.locationentity.canonical', [
+      'locationentity' => $this->entity->id(),
+    ]);
   }
 
 }

9d81c054baff92704e03dbfc06505860fc1b649e
Sierk Beij <sierk@cococollective.org>
2016-12-01 11:31:10 -0800

    Customized LocationDeleteForm class.

--- src/Form/LocationDeleteForm.php
+++ src/Form/LocationDeleteForm.php
@@ -5,10 +5,46 @@ namespace Drupal\locationentity\Form;
 use Drupal\Core\Entity\ContentEntityDeleteForm;
 
 /**
- * Provides a form for deleting Location entities.
+ * Provides a form for deleting locations.
  *
  * @ingroup locationentity
  */
 class LocationDeleteForm extends ContentEntityDeleteForm {
+
+  /**
+   * The entity being used by this form.
+   *
+   * @var \Drupal\locationentity\LocationInterface
+   */
+  protected $entity;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDeletionMessage() {
+    if (!$this->entity->isDefaultTranslation()) {
+      return $this->t('@language translation of location <em>%label</em> (ID: @id) has been deleted.', [
+        '@language' => $this->entity->language()->getName(),
+        '%label' => $this->entity->label(),
+        '@id' => $this->entity->id(),
+      ]);
+    }
+
+    return $this->t('Location <em>%label</em> (ID: @id) has been deleted.', [
+      '%title' => $this->entity->label(),
+      '@id' => $this->entity->id(),
+    ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function logDeletionMessage() {
+    $this->logger('locationentity')
+      ->notice('Deleted location <em>%label</em> (ID: @id).', [
+        '%label' => $this->entity->label(),
+        '@id' => $this->entity->id(),
+      ]);
+  }
 
 }
